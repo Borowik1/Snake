02341422d05772969cbbcee2391d4a0667e7383e
Borowik1 <48255806+Borowik1@users.noreply.github.com>
2020-11-13 21:39:23 +0300

    Работа с памятью

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,17 +10,51 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
 
             Point p1 = new Point(1, 3, '*');
-
-            p1.Draw();
+            Move(p1, 10, 10);
 
             Point p2 = new Point(4, 5, '#');
+            Reset(p2);
 
-            p2.Draw();
 
-            Console.Read();
+
         }
 
+        private static void Reset(Point p2)
+        {
+            p2 = new Point();
+        }
+
+        private static void Move(Point p1, int v1, int v2)
+        {
+            p1.x = p1.x + v1;
+            p1.y = p1.y + v2;
+        }
+
+        private static void Func3(int x)
+        {
+            x += 1;
+        }
+
+        private static void Func2(int value)
+        {
+            value += 1;
+        }
+
+        private static void Func1(int x)
+        {
+        }
     }
 }
